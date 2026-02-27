0c82e6ce2eb053bffea0baa1fd42913716006904
JohanBellander <JohanBellander@users.noreply.github.com>
2026-02-27 21:37:24 +0100

    Initial project setup: TypeScript lib with tsup, vitest, and pnpm

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * vo2maxe - Reverse-engineered Garmin VO2Max estimation algorithm
+ *
+ * Implements both cycling and running VO2Max estimation pipelines
+ * as described in the whitepaper.
+ */
+
+export {};
